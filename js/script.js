075dde9c9140cc302dd96991e98951179b4fd4fa
Alastarte <antpavl17@gmail.com>
2022-11-23 20:51:21 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//alert('hello');
+
+//const result = confirm('Are you here?');
+//console.log(result);
+
+//const answer = +prompt('sosat bydesh?',"18");
+//console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('whats your fuckin name?','');
+answers[1] = prompt('whats your fuckin age?','');
+answers[2] = prompt('whats your fuckin purpose?','');
+
+//document.write(answers);
+
+console.log(typeof(answers));
+
+const category = 'toys';
+
+
+const user = 'ivan';
+
+alert(`privet, ${user}`);
